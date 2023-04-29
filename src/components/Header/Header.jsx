77a7fe8fb7a2d51cdf39c928e2583ca2dd91ea41
Anastasia Sergeeva <lirebook@yandex.ru>
2023-04-29 13:35:40 +0300

    fix: добавить внутр обертку в Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,30 +4,32 @@ import './Header.scss';
 const Header = () => {
     return (
         <header className="header">
-            <nav className="header__nav">
-                <ul className="header__nav-list">
-                    <li className="header__nav-list-item">
-                        <a className="header__nav-list-item-link">Стек</a>
+            <div className="header__content">
+                <nav className="header__nav">
+                    <ul className="header__nav-list">
+                        <li className="header__nav-list-item">
+                            <a className="header__nav-list-item-link">Стек</a>
+                        </li>
+                        <li className="header__nav-list-item">
+                            <a className="header__nav-list-item-link">Проекты</a>
+                        </li>
+                        <li className="header__nav-list-item">
+                            <a className="header__nav-list-item-link">Обо мне</a>
+                        </li>
+                    </ul>
+                </nav>
+                <ul className="header__contacts">
+                    <li className="header__contacts-item">
+                        <a className="header__contacts-link">Telegram</a>
                     </li>
-                    <li className="header__nav-list-item">
-                        <a className="header__nav-list-item-link">Проекты</a>
+                    <li className="header__contacts-item">
+                        <a className="header__contacts-link">WhatsApp</a>
                     </li>
-                    <li className="header__nav-list-item">
-                        <a className="header__nav-list-item-link">Обо мне</a>
+                    <li className="header__contacts-item">
+                        <a className="header__contacts-link">Почта</a>
                     </li>
                 </ul>
-            </nav>
-            <ul className="header__contacts">
-                <li className="header__contacts-item">
-                    <a className="header__contacts-link">Telegram</a>
-                </li>
-                <li className="header__contacts-item">
-                    <a className="header__contacts-link">WhatsApp</a>
-                </li>
-                <li className="header__contacts-item">
-                    <a className="header__contacts-link">Почта</a>
-                </li>
-            </ul>
+            </div>
         </header>
     );
 };
