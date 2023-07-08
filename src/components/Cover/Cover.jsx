1042dbb3fb4a5a98ee431aefabeed5c7c49e4f80
Anastasia Sergeeva <lirebook@yandex.ru>
2023-07-08 15:21:38 +0300

    feat: add new projects, update cv

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -9,7 +9,8 @@ const Cover = ({burgerMenuOpened}) => {
             <img src={Photo} className="cover__image"/>
             <p className="cover__text">Привет! Меня зовут Сергеева Настя 👋</p>
             <h1 className="cover__title">Я - фронтенд-разработчик <br/> Создаю удобные интерфейсы на React</h1>
-            <button className="cover__button">Скачать резюме</button>
+            <a href="https://drive.google.com/file/d/1Oeoj2JPdpRZE2Ya_jvgvXclGgf3l4rte/view?usp=sharing" target="_blank"
+               className="cover__button">Скачать резюме</a>
         </div>
     );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,58 +2,29 @@ import Learn from '../images/how-to-learn-image.png';
 import RussianTravel from '../images/russian-travel-image.png';
 import Movies from '../images/movies-explorer-image.png';
 import Mesto from '../images/mesto-image.png';
+import Give5 from '../images/dai-pyat.png';
+import Portfolio from '../images/portfolio.png';
+import Notes from '../images/notes.png';
+import Moodbeat from '../images/moodbeat.png';
 
 export const projects = [
     {
-        id: 1,
-        title: "Научиться учиться",
-        image: Learn,
-        techs: ['HTML', 'CSS', 'БЭМ', 'анимация', 'flex layout', 'YouTube API', 'доступность'],
-        smallDescription: "Одностраничный лендинг об эффективных способах обучения.",
-        githubLink: "https://github.com/AnaSerg/how-to-learn",
-        link: "https://endearing-brigadeiros-de3204.netlify.app/"
+        id: 8,
+        title: "Настроение сотрудника",
+        image: Moodbeat,
+        techs: ['React', 'SCSS', 'Typescript', 'Storybook', 'Redux', 'vite'],
+        smallDescription: "Приложение для отслеживания уровня выгорания сотрудников и борьбы с ним.",
+        githubLink: "https://github.com/moodbeat/Frontend",
+        link: "https://moodbeat.ru/"
     },
-    /*
     {
-        id: 2,
-        title: "Notes",
-        image: ,
-        techs: [],
-        smallDescription: "Описание",
-        bigDescription: "Длинное описание",
-        githubLink: "",
-        link: ""
-    },
-    */
-    {
-        id: 3,
-        title: "Путешествие по России",
-        image: RussianTravel,
-        techs: ['HTML', 'CSS', 'БЭМ', 'flex layout', 'grid', 'доступность', 'адаптив'],
-        smallDescription: "Лендинг о путешествии по России с перечислением интересных мест, достойных посещения.",
-        githubLink: "https://github.com/AnaSerg/russian-travel",
-        link: "https://anaserg.github.io/russian-travel/"
-    },
-    /*
-    {
-        id: 4,
-        title: "Портфолио",
-        image: ,
-        techs: [],
-        smallDescription: "Описание",
-        bigDescription: "Длинное описание",
-        githubLink: "",
-        link: ""
-    },
-    */
-    {
-        id: 5,
-        title: "Mesto",
-        image: Mesto,
-        techs: ['CSS', 'React', 'Webpack', 'Node.js', 'Express.js', 'MongoDB', 'адаптив'],
-        smallDescription: "Социальная сеть, где можно делиться фотографиями, ставить лайки на фото других пользователей.",
-        githubLink: "https://github.com/AnaSerg/mesto-frontend",
-        link: "https://creative-gumdrop-b6767e.netlify.app/"
+        id: 7,
+        title: "Дай 5!",
+        image: Give5,
+        techs: ['React', 'SCSS', 'webpack', 'адаптив', 'localStorage', 'API', 'анимация'],
+        smallDescription: "Платформа для подготовки к ЕГЭ с возможностью проходить тесты.",
+        githubLink: "https://github.com/AnaSerg/give-five-react/tree/develop",
+        link: "https://daipyat.ru/"
     },
     {
         id: 6,
@@ -64,18 +35,52 @@ export const projects = [
         githubLink: "",
         link: "https://movies-front.nomoredomains.club/"
     },
-    /*
     {
-        id: 7,
-        title: "Дай 5!",
-        image: ,
-        techs: [],
+        id: 5,
+        title: "Портфолио",
+        image: Portfolio,
+        techs: ['Sass', 'React', 'адаптив', 'анимация', 'БЭМ'],
         smallDescription: "Описание",
-        bigDescription: "Длинное описание",
-        githubLink: "",
-        link: "https://daipyat.ru/"
+        githubLink: "https://github.com/AnaSerg/portfolio-website-react",
+        link: "https://portfolio-anaserg.netlify.app/"
     },
-     */
+    {
+        id: 4,
+        title: "Notes",
+        image: Notes,
+        techs: ['HTML', 'CSS', 'JavaScript', 'grid', 'анимация', 'адаптив', 'localStorage'],
+        smallDescription: "https://famous-pixie-8cd736.netlify.app/",
+        githubLink: "https://github.com/AnaSerg/notes-app-vanilla-js",
+        link: "https://famous-pixie-8cd736.netlify.app/"
+    },
+    {
+        id: 3,
+        title: "Mesto",
+        image: Mesto,
+        techs: ['CSS', 'React', 'Webpack', 'Node.js', 'Express.js', 'MongoDB', 'адаптив'],
+        smallDescription: "Социальная сеть, где можно делиться фотографиями, ставить лайки на фото других пользователей.",
+        githubLink: "https://github.com/AnaSerg/mesto-frontend",
+        link: "https://creative-gumdrop-b6767e.netlify.app/"
+    },
+    {
+        id: 2,
+        title: "Путешествие по России",
+        image: RussianTravel,
+        techs: ['HTML', 'CSS', 'БЭМ', 'flex layout', 'grid', 'доступность', 'адаптив'],
+        smallDescription: "Лендинг о путешествии по России с перечислением интересных мест, достойных посещения.",
+        githubLink: "https://github.com/AnaSerg/russian-travel",
+        link: "https://anaserg.github.io/russian-travel/"
+    },
+    {
+        id: 1,
+        title: "Научиться учиться",
+        image: Learn,
+        techs: ['HTML', 'CSS', 'БЭМ', 'анимация', 'flex layout', 'YouTube API', 'доступность'],
+        smallDescription: "Одностраничный лендинг об эффективных способах обучения.",
+        githubLink: "https://github.com/AnaSerg/how-to-learn",
+        link: "https://endearing-brigadeiros-de3204.netlify.app/"
+    }
+
 ]
 
 export const aboutInfo = [
